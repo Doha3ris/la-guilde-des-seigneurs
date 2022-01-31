12580e98e4a8f3e59488d813375dcd3b67d1c63f
PoulpY2K <poulpy2k@gmail.com>
2022-01-31 11:27:51 +0100

    Added CharacterController

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Character;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class CharacterController extends AbstractController
 {
     #[Route('/character/display', name: 'character_display')]
-    public function display(): Response
+    public function display(private Character $character): Response
     {
+        $character->
+
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/CharacterController.php',
